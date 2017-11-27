05882065023244dc5cbcd103c0ba78cde4fb5423
Zachary Wimer <zwimer@gmail.com>
2017-11-27 03:43:17 -0500

    Documentation added

---
+++ target/doc/implementors/core/fmt/trait.UpperHex.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["bitflags"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"bitflags/example_generated/struct.Flags.html\" title=\"struct bitflags::example_generated::Flags\">Flags</a>",];
+implementors["md5"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"md5/struct.Digest.html\" title=\"struct md5::Digest\">Digest</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["md5"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"md5/struct.Digest.html\" title=\"struct md5::Digest\">Digest</a>",];
+implementors["regex_syntax"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"regex_syntax/struct.Lit.html\" title=\"struct regex_syntax::Lit\">Lit</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
